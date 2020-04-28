f64903c38d6d91e5c2fde882471ed8561cbcb656
MiftariB <48824614+MiftariB@users.noreply.github.com>
2020-04-28 16:59:51 +0200

    try.py with comments
    Added comments to know what function does what

--- try.py
+++ try.py
@@ -11,6 +11,34 @@ import time
 env = gym.make("InvertedDoublePendulumBulletEnv-v0")
 env.render(mode="human")
 obs = env.reset()
-while 1:
-	time.sleep(1/60)
-	env.step([10])
+
+
+### GET STATE : 
+print('Initial state '+str(env.robot.calc_state()))
+#return np.array([
+#            x, vx,
+#            self.pos_x,
+#            np.cos(theta), np.sin(theta), theta_dot,
+#            np.cos(gamma), np.sin(gamma), gamma_dot,
+#        ])
+
+time.sleep(1/60)
+
+### step takes action as input 
+#returns state 
+#        sum_of_rewards
+#        Done -> if robot's y position +0.3 <=1
+#        empty {} -> don't know its utility
+state,reward,done,other =  env.step([0.01])
+print('After move state '+ str(state))
+print('reward '+ str(reward))
+print('Done '+str(done))
+print('IDK '+str(other))
+
+### Get ROBOT x and y position
+x_pos = env.robot.pos_x
+y_pos = env.robot.pos_y
+
+print("x pos "+str(x_pos))#X pos shall be the position of the "slider"
+print("y pos "+str(y_pos))#UNKNOWN but could be position of highest point
+
